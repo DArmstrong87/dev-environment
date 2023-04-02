28ff65a2d2951b90009bbd62c6e185876d6d90be
Daniel Armstrong <59901031+DArmstrong87@users.noreply.github.com>
2023-04-02 16:27:16 -0500

    Update installs-wsl.sh

--- scripts/installs-wsl.sh
+++ scripts/installs-wsl.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+# Installs workspace directories
+# WSL updates and install packages
+# Install Oh My Zsh
+# Install Node
+# Install pyenv and python 3.11
+# Install VS Code Extensions
+
 clear
 
 # Set up workspace directory
@@ -13,11 +20,13 @@ if [ ! -d "$FOLDER" ]; then
 else
   echo "Skipping directory creation"
 fi
+echo -e "\n\n"
 
 # WSL Specific: Get latest updates and install packages - May take awhile but needs to run first
 echo -e "\n\nUpdating the Ubuntu operating system..."
 sudo apt-get update && sudo apt-get dist-upgrade -y
 sudo apt install -y curl file build-essential libssl-dev libgtk2.0-0 libgtk-3-0 libgbm-dev libnotify-dev libgconf-2-4 libnss3 libxss1 libasound2 libxtst6 xauth xvfb zsh git fonts-powerline
+echo -e "\n\n"
 
 # Switch to Zsh
 ZSH_FOLDER=$HOME/.oh-my-zsh
@@ -26,6 +35,7 @@ if [ ! -d "$ZSH_FOLDER" ]; then
 else
   echo "Oh My Zsh already installed"
 fi
+echo -e "\n\n"
 
 current_shell=$(echo $SHELL)
 if [ $current_shell == "/bin/bash" ];
@@ -49,6 +59,7 @@ then
 else
     echo "Already using zsh as default shell"
 fi
+echo -e "\n\n"
 
 # Install Node - Needs to run after zsh setup
 if ! command -v nvm &> /dev/null; then
@@ -57,6 +68,7 @@ if ! command -v nvm &> /dev/null; then
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
   source ~/.zshrc &>zsh-reload.log
 fi
+echo -e "\n\n"
 
 nvm install --lts
 nvm use --lts
@@ -67,6 +79,109 @@ echo 'export PATH="$PATH:$HOME/.npm-packages/bin"' >> ~/.zshrc
 source ~/.zshrc &>zsh-reload.log
 npm i -g serve json-server cypress
 # End Node Set up
+echo -e "\n\n"
+
+
+PYTHON_VERSION=3.11
+
+echo "Update Ubuntu and install required packages"
+sudo apt update
+sudo apt install -y gcc make build-essential openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev zlib1g-dev libncursesw5-dev libgdbm-dev libc6-dev zlib1g-dev libsqlite3-dev tk-dev libssl-dev openssl libffi-dev python3 python3-pip wget liblzma-dev curl xz-utils libncurses5-dev python3-openssl llvm sqlite
+
+echo "Install pyenv"
+curl https://pyenv.run | bash
+
+if [ $(cat ~/.zshrc | grep -c 'Configure pyenv') != 1 ];
+then
+  echo Adding Pyenv Configuration
+  echo -e '\n\n# Configure pyenv\n
+  export PYENV_ROOT="$HOME/.pyenv"\n
+  export PIPENV_DIR="$HOME/.local"\n
+  export PATH="$PIPENV_DIR/bin:$PYENV_ROOT/bin:$PATH"\n
+
+  if command -v pyenv 1>/dev/null 2>&1; then\n
+    \texport PATH=$(pyenv root)/shims:$PATH\n
+    \teval "$(pyenv init -)"\n
+  fi\n' >> ~/.zshrc
+  source ~/.zshrc
+else
+  echo Skipping Pyenv Configuration
+fi
+
+
+
+echo "Install pyenv"
+if [ $(pyenv versions | grep -c $PYTHON_VERSION) != 1 ];
+then
+  echo Installing Python version $PYTHON_VERSION
+  pyenv install ${PYTHON_VERSION}:latest
+else
+  echo Skipping $PYTHON_VERSION install
+fi
+
+INSTALLED_PYTHON_VERSION=$(pyenv versions | grep -o ${PYTHON_VERSION}'.*[0-9]' | tail -1)
+pyenv global $INSTALLED_PYTHON_VERSION
+
+
+if [ $(python3 --version | grep -c $INSTALLED_PYTHON_VERSION) != 1 ];
+then
+    echo "Could not set the global python version let an instructor know"
+    return 0
+fi
+
+if [ $(python3 -m pip list | grep -c pipenv) != 1 ];
+then 
+  echo Install pipenv, black and pylint
+  python3 -m pip install pipenv black pylint
+else
+  echo Skipping pipenv, black and pylint install
+fi
+
+if [[ $(which pipenv) == "pipenv not found" ]];
+then
+    echo "Could not find pipenv"
+    return 0
+fi
+echo -e "\n\n"
+
+
+echo "Installing VS Code Extensions"
+code --install-extension ms-python.python --force
+code --install-extension ms-python.vscode-pylance --force
+code --install-extension ms-python.pylint --force
+code --install-extension njpwerner.autodocstring --force
+code --install-extension alexcvzz.vscode-sqlite --force
+code --install-extension streetsidesoftware.code-spell-checker --force
+code --install-extension ms-vscode-remote.remote-wsl --force
+code --install-extension formulahendry.auto-rename-tag --force
+code --install-extension formulahendry.code-runner --force
+code --install-extension mikoz.black-py --force
+code --install-extension aeschli.vscode-css-formatter --force
+code --install-extension batisteo.vscode-django --force
+code --install-extension janisdd.vscode-edit-csv --force
+code --install-extension eamodio.gitlens --force
+code --install-extension oderwat.indent-rainbow --force
+code --install-extension christian-kohler.path-intellisense --force
+code --install-extension timonwong.shellcheck --force
+code --install-extension meganrogge.template-string-converter --force
+code --install-extension samuelcolvin.jinjahtml --force
+
+echo -e "\n\n"
+
+echo "Setting up aliases"
+echo 'alias gs="git status"' >> ~/.zshrc
+echo 'alias gaa="git add --all"' >> ~/.zshrc
+echo 'alias gcm="git commit -m"' >> ~/.zshrc
+echo 'alias gcnb="git checkout -b"' >> ~/.zshrc
+echo 'alias gc="git checkout"' >> ~/.zshrc
+echo 'alias gpo="git push origin"' >> ~/.zshrc
+echo 'alias gplo="git pull origin"' >> ~/.zshrc
+echo 'alias gdb="git branch -d"' >> ~/.zshrc
+echo 'alias pes="pipenv shell"' >> ~/.zshrc
+echo 'alias prs="python manage.py runserver"' >> ~/.zshrc
+echo 'alias psp="python manage.py shell_plus"' >> ~/.zshrc
+
+
 
 echo -e "\n\n\n\n"
 echo "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@"
